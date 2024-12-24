0e09decef672041c6c9a90e20fcb54d1ea039d8d
MHamed <aitsidiouali@gmail.com>
2024-12-25 00:05:25 +0100

    added the drag and drop functionality and the generate computer board logic too

--- src/index.js
+++ src/index.js
@@ -1,79 +1,152 @@
-// import { Ship } from '../src/ship'
-// import { Gameboard } from '../src/gameboard'
-// import { Player } from '../src/player'
+import Ship from '../src/ship'
+import Gameboard from '../src/gameboard'
+import Player from '../src/player'
 
+//create the ships objects 
+let aircraft = new Ship('Aircraft carrier', 5, 0, false)
+let battleship = new Ship('Battleship', 4, 0, false)
+let submarine = new Ship('Submarine', 3, 0, false)
+let destroyer = new Ship('Destroyer', 3, 0, false)
+let patrolBoat = new Ship('Patrol boat', 2, 0, false)
 
-//Aircraft carrier	length: 5
-//Battleship length: 4
-//Submarine length: 3
-//Destroyer legth: 3
-//Patrol boat lenght: 2
+//add all the ships to the queue
+ let ships = [aircraft, battleship, submarine, destroyer, patrolBoat]
+
+//create the player board and the computer board objects
+let PlayerBoard = new Gameboard()
+PlayerBoard.createGameBoard()
+let ComputerBoard = new Gameboard()
+ComputerBoard.createGameBoard()
+
+//create the player and the computer player objects
+let player = new Player(PlayerBoard)
+let computer = new Player(ComputerBoard)
 
 const playerBoard = document.querySelector('.player-board')
 const computerBoard = document.querySelector('.computer-board')
 const modalBoard = document.querySelector('.my-waters')
 
+//get the ships DOM objects
+const aircraftDom = document.querySelector('.aircraft')
+const battleshipDom = document.querySelector('.battleship')
+const submarineDom = document.querySelector('.submarine')
+const destroyerDom = document.querySelector('.destroyer')
+const patrolBoatDom = document.querySelector('.patrol-boat')
 
-function displayBoards() {
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            const playerCard = document.createElement('div')
-            const computerCard = document.createElement('div')
-            const modalCard = document.createElement('div')
-            playerCard.classList.add('player-card')
-            computerCard.classList.add('computer-card')
-            modalCard.classList.add('modal-card')
+const shipsDom = [aircraftDom, battleshipDom, submarineDom, destroyerDom, patrolBoatDom]
 
-            playerBoard.appendChild(playerCard)
-            computerBoard.appendChild(computerCard)
-            modalBoard.appendChild(modalCard)
+function displayComputerBoard() {
+    for (let i = 0; i < ComputerBoard.board.length; i++) {
+        const computerCard = document.createElement('div')
+        computerCard.classList.add('computer-card')
+        computerCard.id = i
+        if (ComputerBoard.board[i].shipName !== null) {
+            computerCard.classList.add('ship-card')
+        } else if (ComputerBoard.board[i].hit === true) {
+            computerCard.classList.add('hit')
+        } else if (ComputerBoard.board[i].missedAttack === true) {
+            computerCard.classList.add('missed-Attack')
         }
+        computerBoard.appendChild(computerCard)
     }
 }
 
-displayBoards()
+function displayPlayerBoard() {
+    for (let i = 0; i < PlayerBoard.board.length; i++) {
+        const playerCard = document.createElement('div')
+        const modalCard = document.createElement('div')
+        playerCard.classList.add('player-card')
+        modalCard.classList.add('modal-card')
+        playerCard.id = i
+        modalCard.id = i
+        if (PlayerBoard.board[i].shipName !== null) {
+            playerCard.classList.add('ship-card')
+            modalCard.classList.add('ship-card')
+        } else if (PlayerBoard.board[i].hit === true) {
+            playerCard.classList.add('hit')
+        } else if (PlayerBoard.board[i].missedAttack === true) {
+            playerCard.classList.add('missed-Attack')
+        }
+        playerBoard.appendChild(playerCard)
+        modalBoard.appendChild(modalCard)
+    }
+}
 
-// let newX = 0, newY = 0, startX = 0, startY = 0
+function removeComputerSquares() {
+    let computerSquares = document.querySelectorAll('.computer-card')
+    computerSquares.forEach(square => computerBoard.removeChild(square))
+}
 
-// playerBoard.addEventListener('mousedown', mouseDown)
+displayComputerBoard()
+displayPlayerBoard()
 
-// function mouseDown(e) {
-//     startX = e.clientX
-//     startY = e.clientY
+function isValidShipPlace(board, ship, startIndex) {
+    let valid = true
+    const upperBound = startIndex + ship.length * 1
+    for (let i = startIndex; i <= upperBound; i++) {
+        if (i % 10 - 1  !== (i - 1) % 10) {
+            valid = false
+        }
+        else if (board.board[i].shipName !== null) {
+            valid = false
+        }
+    }
+    return valid
+}
 
-//     document.addEventListener('mousemove', mouseMove)
-//     document.addEventListener('mouseup', mouseUp)
-// }
+function DomPlayerShipCells(ship, x, Squares) {
+    const len = ship.length
+    for (let i = x; i < x + len; i++) {
+        modalSquares[i].classList.add('ship-card')
+        playerSquares[i].classList.add('ship-card')
+    }
+}
 
-// function mouseMove(e) {
-//     newX = startX - e.clientX
-//     newY = startY - e.clientY
+function placeComputerShip(ship) {
+    const startIndex = Math.floor(Math.random() * (100 - ship.length))
+    if (!isValidShipPlace(ComputerBoard, ship, startIndex)) {
+        placeComputerShip(ship)
+    } else {
+        ComputerBoard.placeShip(ship, startIndex)
+        removeComputerSquares()
+        displayComputerBoard()
+    }
+}
 
-//     startX = e.clientX
-//     startY = e.clientY
+function placePlayerShip(ship, startIndex) {
+    PlayerBoard.placeShip(ship, startIndex)
+    DomPlayerShipCells(ship, startIndex)
+}
 
-//     aircraft.style.top = (aircraft.offsetTop - newX) + 'px'
-//     aircraft.style.left = (aircraft.offsetLeft - newY) + 'px'
-// }
+ships.forEach(ship => placeComputerShip(ship))
 
-// function mouseUp(e) {
-//   document.removeEventListener('mousemove', mouseMove)
-// }
+//drag and drop the ships onto the board
+let draggedShip = null
 
-// aircraft.addEventListener('dragstart', dragStart)
+for (let ship of shipsDom) {
+    ship.addEventListener('dragstart', dragStart)
+}
 
-// let modalCells = document.querySelectorAll('.modal-card')
-// modalCells.forEach(cell => {
-//     cell.addEventListener('dragover', (e) => {
-//         e.preventDefault
-//         cell.style.backgroundColor = 'red'
-//     })
-// })
-// let draggedShip
+function dragStart(e) {
+    draggedShip = e.target
+}
 
-// function dragStart(e) {
-//     draggedShip = e.target
-// }
+const modalSquares = Array.from(modalBoard.children)
+const playerSquares = Array.from(playerBoard.children)
 
+modalSquares.forEach(square => {
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dropShip)
+})
 
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dropShip(e) {
+    e.preventDefault()
+    const startIndex = e.target.id * 1
+    const ship = ships[draggedShip.id]
+    placePlayerShip(ship, startIndex)
+}
 

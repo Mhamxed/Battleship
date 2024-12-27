70dcf1e5f95d05664e38ef907aa23875ad8a7e19
MHamed <aitsidiouali@gmail.com>
2024-12-27 18:24:49 +0100

    moved style from src and index.html from dist to the main dir

--- webpack.config.js
+++ webpack.config.js
@@ -9,18 +9,4 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./dist/index.html",
-      inject: false,
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
 };
